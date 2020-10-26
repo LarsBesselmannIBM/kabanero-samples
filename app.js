6ce3769ab808ec1ecbd9e2c5965499aa450fe812
Lars Besselmann <lars.Besselmann@de.ibm.com>
2020-10-26 18:22:04 +0100

    Vers7

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ module.exports = (/*options*/) => {
 
   app.get('/', (req, res) => {
     // Use req.log (a `pino` instance) to log JSON:
-    req.log.info({message: 'Hello from Appsody! - V7'});
-    res.send('Hello from Appsody! - V7');
+    req.log.info({message: 'Hello from Appsody! - V8'});
+    res.send('Hello from Appsody! - V8');
   });
 
   return app;
